f8c2feb8621aebff9732f74bca6f28228b6157f0
Victor Salles <victor.salles@engenharia.ufjf.br>
2023-06-01 17:04:29 -0300

    Correção na condição do if escrito incorretamente em alguns arquivos

--- challenges/challenge_palindromes_recursive.py
+++ challenges/challenge_palindromes_recursive.py
@@ -1,5 +1,5 @@
 def is_palindrome_recursive(word, low_index, high_index):
-    if word == "" or low_index == None or high_index == None:
+    if word == "" or low_index is None or high_index is None:
         return False
 
     if low_index > high_index:
