e7ac94d9998adafbcae96d0c763cf735cc7d3759
Victor Salles <victor.salles@engenharia.ufjf.br>
2023-06-01 14:58:21 -0300

    Requisito 1

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,3 +1,20 @@
 def study_schedule(permanence_period, target_time):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    if target_time == None:
+        return None
+
+    count = 0
+    for time in permanence_period:
+        if (
+            type(time) != tuple
+            or len(time) != 2
+            or type(time[0]) != int
+            or type(time[1]) != int
+            or time[0] > time[1]
+            or time[0] < 0
+            or time[1] < 0
+        ):
+            return None
+        elif time[0] <= target_time <= time[1]:
+            count += 1
+
+    return count
