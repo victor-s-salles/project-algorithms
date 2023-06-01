fd1859940a6c31d6aedc46c9c02afb4a32577494
Victor Salles <victor.salles@engenharia.ufjf.br>
2023-06-01 16:01:56 -0300

    Requisito 3

--- challenges/challenge_palindromes_recursive.py
+++ challenges/challenge_palindromes_recursive.py
@@ -1,3 +1,10 @@
 def is_palindrome_recursive(word, low_index, high_index):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    if word == "" or low_index == None or high_index == None:
+        return False
+
+    if low_index > high_index:
+        return True
+    if word[low_index].lower() != word[high_index].lower():
+        return False
+
+    return is_palindrome_recursive(word, low_index + 1, high_index - 1)
