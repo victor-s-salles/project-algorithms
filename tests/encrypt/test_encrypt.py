7c3a1e0ffbad7e1dc43ef87c27b116a9a1dc92ba
Victor Salles <victor.salles@engenharia.ufjf.br>
2023-06-01 15:54:41 -0300

    Requisito 2 (incompleto)

--- tests/encrypt/test_encrypt.py
+++ tests/encrypt/test_encrypt.py
@@ -1,5 +1,13 @@
-# from challenges.challenge_encrypt_message import encrypt_message
+from challenges.challenge_encrypt_message import encrypt_message
+import pytest
 
 
 def test_encrypt_message():
-    pass
+    with pytest.raises(TypeError, match="tipo inválido para key"):
+        encrypt_message("Victor", "C")
+    with pytest.raises(TypeError, match="tipo inválido para message"):
+        encrypt_message(12, 5)
+
+    assert encrypt_message("victor", 3) == "civ_rot"
+    assert encrypt_message("victor", 2) == "rotc_iv"
+    assert encrypt_message("victor", 6) == "rotciv"
