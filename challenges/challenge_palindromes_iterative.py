1fffddaa59142c5d038633f7ae952cabffb67dc2
Victor Salles <victor.salles@engenharia.ufjf.br>
2023-06-02 14:55:19 -0300

    Requisito 6

--- challenges/challenge_palindromes_iterative.py
+++ challenges/challenge_palindromes_iterative.py
@@ -1,3 +1,9 @@
 def is_palindrome_iterative(word):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    if word is None or len(word) == 0:
+        return False
+
+    reversed_word = word[::-1]
+
+    if reversed_word.lower() == word.lower():
+        return True
+    return False
