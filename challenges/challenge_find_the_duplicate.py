d57d5c3d0c029cbe2ed44bfa4dbe4107cd2b1992
Victor Salles <victor.salles@engenharia.ufjf.br>
2023-06-02 14:50:17 -0300

    Requisito 5

--- challenges/challenge_find_the_duplicate.py
+++ challenges/challenge_find_the_duplicate.py
@@ -1,3 +1,9 @@
 def find_duplicate(nums):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    nums_sorted = sorted(nums)
+    try:
+        for n in range(len(nums_sorted) - 1):
+            if nums_sorted[n] == nums_sorted[n + 1] and nums_sorted[n] >= 1:
+                return nums_sorted[n]
+        return False
+    except TypeError:
+        return False
